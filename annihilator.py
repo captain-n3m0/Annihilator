f1dc49b2ecea97cc77e1dc6f4693eb67811e8644
captain-n3m0 <techtubebydebjit@gmail.com>
2023-04-24 15:49:28 +0530

    improved code to annihilate the file completely.

--- annihilator.py
+++ annihilator.py
@@ -1,27 +1,38 @@
 import os
+import random
 import shutil
 
 def shred_file(file_path, passes=3):
     """Securely shreds a file by overwriting its content multiple times."""
-    with open(file_path, 'rb+') as file:
-        file_size = os.path.getsize(file_path)
-        file_content = bytearray(file.read())
+    file_size = os.path.getsize(file_path)
 
-        # Overwrite the file content with random bytes multiple times
-        for i in range(passes):
-            file.seek(0)
-            shutil.rmtree(file_path)
-            file.write(os.urandom(file_size))
+    # Overwrite the file content with multiple overwriting patterns
+    for i in range(passes):
+        with open(file_path, 'rb+') as file:
+            # Generate random bytes using a cryptographically secure random number generator
+            random_generator = random.SystemRandom() if os.name == 'posix' else random.Random()
+            file_content = bytearray(random_generator.getrandbits(8) for _ in range(file_size))
+
+            # Write multiple overwriting patterns to the file
+            patterns = [bytearray(file_size), bytearray([0xFF] * file_size), bytearray([0x00] * file_size)]
+            file.write(patterns[i % len(patterns)])
             file.flush()
             os.fsync(file.fileno())
 
-        # Set the file size to 0 and truncate the file
+    # Set the file size to 0 and truncate the file
+    with open(file_path, 'wb') as file:
         file.truncate(0)
         file.flush()
         os.fsync(file.fileno())
 
+    # Rename the file to a random name and wipe its metadata
+    file_name, file_ext = os.path.splitext(file_path)
+    random_name = ''.join(random.choice('0123456789ABCDEF') for _ in range(16)) + file_ext
+    os.rename(file_path, random_name)
+    os.utime(random_name, (0, 0))
+
     # Remove the file
-    os.remove(file_path)
+    shutil.rmtree(random_name)
     print("File securely shredded: {}".format(file_path))
 
 # Get user input for file path and number of passes
